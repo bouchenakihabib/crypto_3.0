23520a4053ef73aa31ae1bdcb61f5c608c8133e6
habibbouchenaki <bouchenaki.habib@yahoo.fr>
2022-04-24 14:24:54 +0200

    last changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
             </div>
             <div className='flex justify-center items-center flex-col empty-5'>
                 <p className='text-white text-small text-center'>Join Us</p>
-                <p className='text-white text-small text-center'>contact@bhdev.com</p>
+                <p className='text-white text-small text-center'>contact@bouchenaki-habib.com</p>
             </div>
             <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5'/>
             <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
